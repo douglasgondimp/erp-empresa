88c848520d51b0aeebc593dc2ef16fc72c0b62a8
Douglas Gondim <douglasgondimp@gmail.com>
2024-03-22 14:59:10 -0300

    (fix:api) adiciona index na tabela companies

--- api/database/migrations/2024_03_22_135436_create_companies_table.php
+++ api/database/migrations/2024_03_22_135436_create_companies_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->timestamps();
 
             $table->primary(['recnum','codigo']);
+            $table->index(['codigo']);
         });
     }
 
