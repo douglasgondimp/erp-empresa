1e42d530f73872375b3c92830fb10b77b20b7c80
Douglas Gondim <douglasgondimp@gmail.com>
2024-03-22 19:45:54 -0300

    (fix:api) altera coluna `client_id` para `nullable` na tabela `users`

--- api/database/migrations/2024_03_22_142141_create_clients_table.php
+++ api/database/migrations/2024_03_22_142141_create_clients_table.php
@@ -26,7 +26,7 @@ return new class extends Migration
         });
 
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedBigInteger('client_id');
+            $table->unsignedBigInteger('client_id')->nullable();
             $table->foreign('client_id')->references('recnum')->on('clients');
         });
     }
