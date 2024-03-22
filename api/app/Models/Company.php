b4745bc4950b0a4f98c8cde7ae9cd7de45c7367b
Douglas Gondim <douglasgondimp@gmail.com>
2024-03-22 15:13:51 -0300

    (feat:api) adiciona relacionamento na model Company

--- api/app/Models/Company.php
+++ api/app/Models/Company.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Company extends Model
 {
@@ -15,4 +17,9 @@ class Company extends Model
         'sigla', // varchar(40)
         'razao_social' // varchar(255)
     ];
+
+    public function clients(): HasMany
+    {
+        return $this->hasMany(Client::class, 'empresa', 'codigo');
+    }  
 }
