0e7274bae82a02c57ab2a08dc2142186ffa8fcc8
Douglas Gondim <douglasgondimp@gmail.com>
2024-03-22 17:59:45 -0300

    (feat:api) adiciona relacionamento com usuário

--- api/app/Models/Client.php
+++ api/app/Models/Client.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Client extends Model
 {
@@ -21,5 +22,10 @@ class Client extends Model
     public function company(): BelongsTo
     {
         return $this->belongsTo(Company::class, 'empresa', 'codigo');
-    } 
+    }
+
+    public function user(): HasOne
+    {
+        return $this->hasOne(User::class, 'client_id', 'recnum');
+    }
 }
