6f466396139f9708f50812c566854a7c8d876a6c
Douglas Gondim <douglasgondimp@gmail.com>
2024-03-23 20:21:36 -0300

    (feat:api) criar rota para verificar usuário autenticado

--- api/routes/api.php
+++ api/routes/api.php
@@ -5,10 +5,11 @@ use App\Http\Controllers\ClientController;
 use App\Http\Controllers\CompanyController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/auth/login', [AuthController::class, 'login'])->name('auth.login');
+Route::post('/auth/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/auth/logout', [AuthController::class, 'logout'])->name('auth.logout');
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
+    Route::post('/auth/check', [AuthController::class, 'checkAuth']);
 
     Route::apiResource('companies', CompanyController::class);
     Route::apiResource('clients', ClientController::class);
