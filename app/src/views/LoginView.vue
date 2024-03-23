dc9273dd4c13738ef848cc53c51a0d9abf90bc84
Douglas Gondim <douglasgondimp@gmail.com>
2024-03-23 20:25:25 -0300

    (feat:app) cria a view LoginView.vue

---
+++ app/src/views/LoginView.vue
@@ -0,0 +1,33 @@
+<script setup>
+import {reactive} from 'vue';
+import { useStore } from 'vuex';
+
+const store = useStore();
+const user = reactive({
+    email: '',
+    password: ''
+});
+
+function login()
+{
+    store.dispatch('login', user);
+}
+</script>
+
+<template>
+    <div>
+        <h1 class="text-3xl font-bold underline">
+            Página de Login
+        </h1>
+
+        <form @submit.prevent="login">
+            <input type="email" placeholder="Email" v-model="user.email">
+            <input type="password" placeholder="Senha" v-model="user.password">
+            <button type="submit">Entrar</button>
+        </form>
+    </div>
+</template>
+
+<style scoped>
+    
+</style>
