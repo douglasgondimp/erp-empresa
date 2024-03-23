7b94290383e5659996bc5c32143acb4a0734c501
Douglas Gondim <douglasgondimp@gmail.com>
2024-03-23 12:08:38 -0300

    (feat:api) cria rotas para o login e rota resource para companies

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,5 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CompanyController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/auth/login');
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/auth/login', 'login')->name('auth.login');
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('companies', CompanyController::class);
+});
