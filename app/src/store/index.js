f6fee764a0d7eeecfab84d790a3710309829a9bd
Douglas Gondim <douglasgondimp@gmail.com>
2024-03-23 20:26:53 -0300

    (feat:app) cria a store com alguns métodos referentes a login e token

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -1,17 +1,43 @@
 import { createStore } from 'vuex'
-import axios from 'axios'
+import http from '@/services/http.js';
 
 export default createStore({
     state: {
-
+        user: {},
+        token: ''
     },
     mutations: {
-
+        setUser (state, user) {
+            state.user = user;
+            localStorage.setItem('user', JSON.stringify(user));
+        },
+        setToken (state, token) {
+            state.token = token;
+            localStorage.setItem('token', token);
+        }
     },
     actions: {
-
-    }
-    modules: {
-        
-    }
+        async login({commit}, user) {
+            try {
+                await http.post('/auth/login', user)
+                .then(response => {
+                    commit('setUser', response.data.user);
+                    commit('setToken', response.data.token);
+                    console.log(response.data);
+                })
+            } catch (error) {
+                console.error(error);
+            }
+        },
+        getToken({commit}) {
+            if (localStorage.getItem('token')) {
+                commit('setToken', localStorage.getItem('token'));
+            }
+        },
+        getUser({commit}) {
+            if (localStorage.getItem('user')) {
+                commit('setUser', JSON.parse(localStorage.getItem('user')));
+            }
+        }
+    },
 })
