dafc842421c4a95c1db31a8b1a03dc59c3888cf0
Douglas Gondim <douglasgondimp@gmail.com>
2024-03-23 16:00:10 -0300

    (feat:api) cria rota resource para os clientes

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ClientController;
 use App\Http\Controllers\CompanyController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,4 +11,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/auth/logout', [AuthController::class, 'logout'])->name('auth.logout');
 
     Route::apiResource('companies', CompanyController::class);
+    Route::apiResource('clients', ClientController::class);
 });
