ff4c1e490554235e1452d764f709ac0cadf933e6
Douglas Gondim <douglasgondimp@gmail.com>
2024-03-23 12:29:00 -0300

    (feat:api) ria rota para o usuário deslogar no sistema

--- api/routes/api.php
+++ api/routes/api.php
@@ -4,10 +4,10 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CompanyController;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(AuthController::class)->group(function () {
-    Route::post('/auth/login', 'login')->name('auth.login');
-});
+Route::post('/auth/login', [AuthController::class, 'login'])->name('auth.login');
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/auth/logout', [AuthController::class, 'logout'])->name('auth.logout');
+
     Route::apiResource('companies', CompanyController::class);
 });
