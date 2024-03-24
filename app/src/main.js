64e6a0c0438ad54d953646deead618bda71abb4d
Douglas Gondim <douglasgondimp@gmail.com>
2024-03-24 11:04:20 -0300

    (feat:app) adiciona pacote do primevue na aplicação

--- app/src/main.js
+++ app/src/main.js
@@ -4,9 +4,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/aura-dark-teal/theme.css';
 
 const app = createApp(App)
 
 app.use(router)
 app.use(store)
+app.use(PrimeVue);
 app.mount('#app')
