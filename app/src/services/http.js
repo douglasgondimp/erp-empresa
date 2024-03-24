0edceb443d86e8abb75d88ea4ce6c987e375d198
Douglas Gondim <douglasgondimp@gmail.com>
2024-03-24 16:40:48 -0300

    (fix:api) remove injeção de token com mapState na service

--- app/src/services/http.js
+++ app/src/services/http.js
@@ -1,18 +1,10 @@
 import axios from 'axios';
 import { computed } from 'vue';
-import { mapState } from 'vuex';
-
-const state = computed(() => {
-    return mapState([
-        'token'
-    ])
-});
 
 const axiosInstance = axios.create({
     baseURL: 'http://localhost/api',
     headers: {
         'Content-type': 'application/json',
-        'Authorization': `Bearer ${state.token ?? ''}`
     }
 });
 
