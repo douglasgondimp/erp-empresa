cc5a451e4541c059e194d373ef41e1f6ee640baa
Douglas Gondim <douglasgondimp@gmail.com>
2024-03-24 16:40:07 -0300

    (feat:api) aplica componentes e estilização na página de login

--- app/src/views/LoginView.vue
+++ app/src/views/LoginView.vue
@@ -1,6 +1,11 @@
 <script setup>
-import {reactive} from 'vue';
+import { reactive } from 'vue';
 import { useStore } from 'vuex';
+import InputGroup from 'primevue/inputgroup';
+import InputGroupAddon from 'primevue/inputgroupaddon';
+import InputText from 'primevue/inputtext';
+import Password from 'primevue/password';
+import Button from 'primevue/button';
 
 const store = useStore();
 const user = reactive({
@@ -15,19 +20,35 @@ function login()
 </script>
 
 <template>
-    <div>
-        <h1 class="text-3xl font-bold underline">
-            Página de Login
-        </h1>
-
-        <form @submit.prevent="login">
-            <input type="email" placeholder="Email" v-model="user.email">
-            <input type="password" placeholder="Senha" v-model="user.password">
-            <button type="submit">Entrar</button>
-        </form>
+    <div class="h-screen flex flex-col justify-center items-center">
+        <section class="box_form_login bg-slate-200 p-4 rounded shadow-2xl">
+            <h1 class="text-xl font-bold text-center">
+                Login
+            </h1>
+            <Toast />
+            <form @submit.prevent="login" class="mt-3 block">
+                <InputGroup>
+                    <InputGroupAddon class="bg-black">
+                        <i class="pi pi-user"></i>
+                    </InputGroupAddon>
+                    <InputText type="email" placeholder="Email do usuário" v-model="user.email" required />
+                </InputGroup>
+                <InputGroup class="mt-3">
+                    <InputGroupAddon class="bg-black">
+                        <i class="pi pi-user"></i>
+                    </InputGroupAddon>
+                    <Password placeholder="Sua senha" v-model="user.password" :feedback="false" required/>
+                </InputGroup>
+                <div class="text-center mt-3">
+                    <Button type="submit" label="Entrar" severity="success" class="bg-emerald-500 py-0.5 px-2" @click="showSuccess" />
+                </div>
+            </form>
+        </section>
     </div>
 </template>
 
-<style scoped>
-    
+<style scoped lang="scss">
+    .box_form_login {
+        width: 400px;
+    }
 </style>
