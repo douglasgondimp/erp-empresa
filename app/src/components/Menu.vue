c302ed2d9f4a426c31a25dcc51d35bce331c396b
Douglas Gondim <douglasgondimp@gmail.com>
2024-03-23 22:58:13 -0300

    (feat:app) cria o componente Menu.vue

---
+++ app/src/components/Menu.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <nav>
+        <ul>
+            <li>
+                <router-link :to="{ name: 'clients' }">Clientes</router-link>
+            </li>
+            <li>
+                <router-link :to="{ name: 'companies' }">Empresas</router-link>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<style scoped>
+
+</style>
