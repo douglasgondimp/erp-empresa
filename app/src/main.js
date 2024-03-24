d4f76dbaaa6b527fb6566d6192e764227a67565b
Douglas Gondim <douglasgondimp@gmail.com>
2024-03-24 16:39:38 -0300

    (feat:app) adiciona icones, tema e componentes do primevue

--- app/src/main.js
+++ app/src/main.js
@@ -1,15 +1,19 @@
 import './style.css'
+import 'primeicons/primeicons.css'
+import 'primevue/resources/themes/aura-light-teal/theme.css';
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import PrimeVue from 'primevue/config';
-import 'primevue/resources/themes/aura-dark-teal/theme.css';
+import Toast from 'primevue/toast';
+
 
 const app = createApp(App)
 
 app.use(router)
 app.use(store)
-app.use(PrimeVue);
+app.use(PrimeVue)
+app.use(Toast)
 app.mount('#app')
