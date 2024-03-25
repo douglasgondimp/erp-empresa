d78671a722b742edaed3c0fd06dbb70632f451e5
Douglas Gondim <douglasgondimp@gmail.com>
2024-03-25 13:24:19 -0300

    (feat:app) adiciona `softDeletes` nos clientes e empresas

--- api/app/Models/Client.php
+++ api/app/Models/Client.php
@@ -10,10 +10,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Client extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $primaryKey = 'codigo';
 
