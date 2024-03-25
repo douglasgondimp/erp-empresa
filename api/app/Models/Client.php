7be7c319673ce80f4f54162ec27e38dd83967531
Douglas Gondim <douglasgondimp@gmail.com>
2024-03-25 10:27:19 -0300

    (feat:api) cria método de atributo para cpf_cnpj no cliente

--- api/app/Models/Client.php
+++ api/app/Models/Client.php
@@ -2,7 +2,10 @@
 
 namespace App\Models;
 
+use App\Helpers\Helpers;
+use App\Helpers\MaskHelper;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -33,6 +36,14 @@ class Client extends Model
         static::addGlobalScope('company', function (Builder $builder) {
             $builder->with('company');
         });
+    }
+
+    public function cpfCnpj(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => MaskHelper::maskCPFCNPJ($value),
+            set: fn (string $value) => Helpers::getNumbers($value),
+        );
     }
 
     public function company(): BelongsTo
